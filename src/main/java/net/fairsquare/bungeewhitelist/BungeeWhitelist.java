eca64350480c0f650135ae139b77672a9c5f129c
McJeffr <mcjeffr@gmail.com>
2020-07-10 12:49:43 +0200

    Initial commit

---
+++ src/main/java/net/fairsquare/bungeewhitelist/BungeeWhitelist.java
@@ -0,0 +1,17 @@
+package net.fairsquare.bungeewhitelist;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungeeWhitelist extends Plugin {
+
+    @Override
+    public void onEnable() {
+        //TODO: Startup code
+    }
+
+    @Override
+    public void onDisable() {
+        //TODO: Shutdown code
+    }
+
+}
